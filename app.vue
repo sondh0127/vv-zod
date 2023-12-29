f9b969f6348a6d5734c4a7fefdad70d5d2c8bf37
Son Hong Do <sondh0127@gmail.com>
2023-12-29 11:12:14 +0700

    vee-repro

---
+++ app.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import { z } from 'zod'
+import { toTypedSchema } from '@vee-validate/zod'
+
+
+const schema = z.object({
+  id: z.string()
+}).superRefine(() => {})
+
+const { handleSubmit, meta, values, errors, resetForm } = useForm({
+  initialValues: {
+    id: ''
+  },
+  validationSchema: toTypedSchema(schema),
+  keepValuesOnUnmount: true,
+})
+</script>
+
+<template>
+  <div>
+    <Field name="id" v-slot="{ meta }">
+      <details open>
+        <summary>meta.required</summary>
+        <pre>{{ JSON.stringify(meta.required, null, 2) }}</pre>
+      </details>
+    </Field>
+  </div>
+</template>
